2e98dc110c1c25cacf394b7fedd9ec4474b23a20
agladyshev <agladyshev@inat.ai>
2021-01-28 21:22:20 +0300

    refactor project structure

---
+++ src/benchmarking.py
@@ -0,0 +1,64 @@
+from argparse import ArgumentParser
+
+from models.ssd.utils import dboxes300_coco, Encoder
+from models.ssd.evaluate import evaluate
+from models.ssd.data import get_val_dataset, get_val_dataloader, get_coco_ground_truth
+
+from common_parameters import CommonParameters
+from utils.utils_common import generate_model
+
+
+def make_parser():
+    parser = ArgumentParser(description="Train Single Shot MultiBox Detector"
+                                        " on COCO")
+    parser.add_argument('--data', '-d', type=str, default='/coco', required=False,
+                        help='path to test and training data files')
+    parser.add_argument('--eval-batch-size', '--ebs', type=int, default=32,
+                        help='number of examples for each evaluation iteration')
+    parser.add_argument('--no-cuda', action='store_true',
+                        help='use available GPUs')
+    parser.add_argument('--checkpoint', type=str, default=None,
+                        help='path to model checkpoint file')
+
+    parser.add_argument('--num-workers', type=int, default=4)
+    parser.add_argument('--amp', action='store_true',
+                        help='Whether to enable AMP ops. When false, uses TF32 on A100 and FP32 on V100 GPUS.')
+
+    return parser
+
+
+def bench(args, parameters):
+    # Setup data, defaults
+    dboxes = dboxes300_coco()
+    encoder = Encoder(dboxes)
+    cocoGt = get_coco_ground_truth(args)
+
+    val_dataset = get_val_dataset(args)
+    val_dataloader = get_val_dataloader(val_dataset, args)
+
+    ssd300 = generate_model(parameters)
+
+    inv_map = {v: k for k, v in val_dataset.label_map.items()}
+
+    acc = evaluate(ssd300, val_dataloader, cocoGt, encoder, inv_map, args)
+    print('Model precision {} mAP'.format(acc))
+
+
+def main():
+    parser = make_parser()
+    args = parser.parse_args()
+    common_parameters = CommonParameters()
+    common_parameters.load_parameters()
+
+    args.data = common_parameters.coco_data_path
+    args.checkpoint = common_parameters.weights_path
+    args.amp = common_parameters.use_fp16_mode
+    args.no_cuda = False
+    args.eval_batch_size = common_parameters.eval_batch_size
+    args.num_workers = common_parameters.num_workers
+
+    bench(args, common_parameters)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/models/ssd/processing_utils.py
@@ -0,0 +1,47 @@
+import cv2
+import torch
+import numpy as np
+
+from models.ssd.utils import dboxes300_coco, Encoder
+from common_parameters import CommonParameters
+
+
+def padding_by_zeros(image: np.ndarray) -> np.ndarray:
+    h, w, c = image.shape
+    max_size = max(w, h)
+    image_pad = np.zeros((max_size, max_size, c), dtype=np.uint8)
+    image_pad[:h, :w, :] = image
+    return image_pad
+
+
+def preprocess_input(image: np.ndarray, parameters: CommonParameters) -> torch.Tensor:
+    model_input = cv2.resize(image, (parameters.target_width, parameters.target_height))
+    model_input = (model_input - parameters.norm_mean) / parameters.norm_std
+    model_input = np.rollaxis(model_input, 2, 0)
+
+    model_input = torch.from_numpy(np.array([model_input]))
+    model_input = model_input.float()
+    if parameters.use_fp16_mode:
+        model_input = model_input.half()
+    model_input = model_input.to(parameters.device)
+    return model_input
+
+
+def decode_results(predictions):
+    dboxes = dboxes300_coco()
+    encoder = Encoder(dboxes)
+    ploc, plabel = [val.float() for val in predictions]
+    results = encoder.decode_batch(ploc, plabel, criteria=0.5, max_output=20)
+    return [[pred.detach().cpu().numpy() for pred in detections] for detections in results]
+
+
+def pick_best(detections, threshold=0.3):
+    bboxes, classes, confidences = detections
+    best = np.argwhere(confidences > threshold)[:, 0]
+    return [pred[best] for pred in detections]
+
+
+def get_coco_object_dictionary(names_path):
+    class_names = open(names_path).readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names
